6a4bb8f9b39d1447576205996e6ff91d5e8aa198
Christine Theobald <christinetheobald@icloud.com>
2020-05-16 11:47:22 -0400

    fixed routing signIn and signOut

--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'SignIn'
+      route: 'SignIn',
+      isSignedIn: false
     }
   }
 
@@ -51,7 +52,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({box});
   }
   
@@ -71,17 +71,23 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
     <div className="App">
         <Particles className='particles'
               params={particlesOptions}
             />
-      <Navigation onRouteChange={this.onRouteChange} />
-      { this.state.route === 'home'
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+      { route === 'home'
           ? <div>
               <Logo />
               <Rank />
@@ -89,16 +95,13 @@ class App extends Component {
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
             </div>
           : (
-            this.state.route === 'SignIn' 
+            route === 'SignIn' 
             ? <SignIn onRouteChange={this.onRouteChange}/>
             : <Register onRouteChange={this.onRouteChange}/>
-            )
-
-          
-        
+            )     
       }
     </div>
   );
